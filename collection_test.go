bf69b1c882abd128e48ca4e4ff2d492ae8b41f11
Habibi Mustafa <bibitahabibi@gmail.com>
2021-09-14 08:56:55 +0700

    when has empty keys then should be false

--- collection_test.go
+++ collection_test.go
@@ -149,12 +149,14 @@ func TestCollectionHas(t *testing.T) {
 	strCollection := Collect(arrString)
 	assert.True(t, strCollection.Has(2))
 	assert.True(t, strCollection.Has(2, 4))
+	assert.False(t, strCollection.Has())
 	assert.False(t, strCollection.Has(8))
 	assert.False(t, strCollection.Has(2, 8))
 
 	mapCollection := Collect(arrMap)
 	assert.True(t, mapCollection.Has("First Name"))
 	assert.True(t, mapCollection.Has("First Name", "Last Name"))
+	assert.False(t, mapCollection.Has())
 	assert.False(t, mapCollection.Has("City"))
 	assert.False(t, mapCollection.Has("First Name", "City"))
 }
